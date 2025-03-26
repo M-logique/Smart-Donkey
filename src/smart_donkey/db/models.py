853eb16bef9634088e4d8ffbbd9d2125a4cefae4
M-logique <lmy615730@gmail.com>
2025-03-27 00:19:31 +0330

    Remove unused BigBigInteger import from database models

--- src/smart_donkey/db/models.py
+++ src/smart_donkey/db/models.py
@@ -1,8 +1,8 @@
 from enum import Enum as PyEnum
 from typing import List, Tuple
 
-from sqlalchemy import (JSON, TIMESTAMP, Boolean, Enum, ForeignKey, BigInteger, Text,
-                        BigBigInteger,
+from sqlalchemy import (JSON, TIMESTAMP, Boolean, Enum, ForeignKey, Text,
+                        BigInteger,
                         UniqueConstraint, func)
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
